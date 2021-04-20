294749a223a5d66ed19354c91c2bcbe82fc7a527
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-04-20 12:12:29 +0100

    Add integration specs
    So we can perform higher level
    integration tests, similar to feature
    test but for an library.

---
+++ spec/integrations/get_max_distance_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'google-maps-directions'
+require 'support/directions_stubs'
+
+RSpec.describe 'Get distances for journey', type: :feature do
+  let(:client) { GoogleMaps::Directions::Client.new }
+  let(:options) { { region: 'uk', alternatives: true } }
+  let(:result) { client.directions(origin: 'disneyland', destination: 'neverland', **options) }
+
+  context 'with single route with one leg', valid_response: true do
+    specify 'max distance same as min distance' do
+      longest = result.distances.max
+      shortest = result.distances.min
+      expect(longest).to be shortest
+      expect(longest.value).to be shortest.value
+    end
+  end
+
+  context 'with mutiple routes with one leg each', valid_response_with_alternatives: true do
+    specify 'max distance different from min distance' do
+      longest = result.distances.max
+      shortest = result.distances.min
+      expect(longest).not_to be(shortest)
+      expect(longest.value).not_to be(shortest.value)
+    end
+  end
+
+  # context 'with single route with multiple legs each' do
+  # end
+
+  # context 'with mutiple routes with multiple legs each' do
+  # end
+end

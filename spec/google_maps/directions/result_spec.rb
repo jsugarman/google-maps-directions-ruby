c9be69d9814e877d13eedb42357ee6daf042642b
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-04-27 21:12:26 +0100

    Spec #fetch and #[] unhappy path

--- spec/google_maps/directions/result_spec.rb
+++ spec/google_maps/directions/result_spec.rb
@@ -18,6 +18,30 @@ RSpec.describe GoogleMaps::Directions::Result do
 
       it { is_expected.to eql('OK') }
     end
+
+    context 'when accessing non-existent key' do
+      let(:key) { 'foo' }
+
+      it { is_expected.to be_nil }
+    end
+  end
+
+  describe '#fetch' do
+    subject { result.fetch(key, 'bar') }
+
+    let(:response) { '{ "status" : "OK" }' }
+
+    context 'when accessing existing key' do
+      let(:key) { 'status' }
+
+      it { is_expected.to eql('OK') }
+    end
+
+    context 'when accessing non-existent key' do
+      let(:key) { 'foo' }
+
+      it { is_expected.to eql('bar') }
+    end
   end
 
   describe '#success?' do
